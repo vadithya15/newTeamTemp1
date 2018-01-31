478137fd8c59bc65e9d0782fec6428c347732e4b
Ravindra Prasad <ravindraprasad@yahoo.com>
2018-01-31 14:55:01 +0530

    Update dev.py

--- teamtemp/settings/dev.py
+++ teamtemp/settings/dev.py
@@ -5,7 +5,7 @@ from teamtemp.settings.base import *
 DEBUG = os.environ.get('DJANGO_DEBUG', True)
 SECRET_KEY = os.environ.get(
     'TEAMTEMP_SECRET_KEY',
-    'rp47vufz8lrr1cxki7lmc9w221ajgauk5ctv6xi')
+    'rp47vufz8lrr1cxki7lmc9w221ajgauk5ctv6xj')
 
 DATABASES = {
     'default': {
