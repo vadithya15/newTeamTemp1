aabfc382376ebf3a949b48f08f10b3dbdf0a3caf
Ravindra Prasad <ravindraprasad@yahoo.com>
2018-02-03 23:18:32 +0530

    Update wsgi.py

--- teamtemp/wsgi.py
+++ teamtemp/wsgi.py
@@ -1,5 +1,8 @@
+import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "teamtemp.settings")
 
 application = DjangoWhiteNoise(get_wsgi_application())
 
